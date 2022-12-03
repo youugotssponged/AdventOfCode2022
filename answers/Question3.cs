101b343575b83c17700e226948580db1e138101b
Jordan McCann <jordanmccann64@outlook.com>
2022-12-03 20:14:20 +0000

    Question 3 part 2

--- answers/Question3.cs
+++ answers/Question3.cs
@@ -2,34 +2,34 @@ namespace AOC2022;
 
 public class Question3 : IAnswer
 {
+    public char[] LowerCasePriorities = {
+        'a', 'b', 'c', 'd',
+        'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x',
+        'y', 'z'
+    };
+
     public int ProcessAnswer()
     {
-        char[] LowerCasePriorities = {
-            'a', 'b', 'c', 'd', 
-            'e', 'f', 'g', 'h', 
-            'i', 'j', 'k', 'l', 
-            'm', 'n', 'o', 'p', 
-            'q', 'r', 's', 't', 
-            'u', 'v', 'w', 'x', 
-            'y', 'z' 
-        };
-
         var lines = Util.LoadQuestionFile("question3.txt");
         List<char> DuplicateItems = new();
 
-        foreach(var line in lines)
+        foreach (var line in lines)
         {
             var backpackFirstSlot = line.Substring(0, line.Length / 2);
             var backpackSecondSlot = line.Substring(line.Length / 2);
             bool foundForLine = false;
 
-            for(int i = 0; i < backpackFirstSlot.Length; i++)
+            for (int i = 0; i < backpackFirstSlot.Length; i++)
             {
-                if(foundForLine) break;
+                if (foundForLine) break;
 
-                for(int j = 0; j < backpackSecondSlot.Length; j++)
+                for (int j = 0; j < backpackSecondSlot.Length; j++)
                 {
-                    if(backpackFirstSlot[i] == backpackSecondSlot[j]) 
+                    if (backpackFirstSlot[i] == backpackSecondSlot[j])
                     {
                         DuplicateItems.Add(backpackSecondSlot[j]);
                         foundForLine = true;
@@ -39,19 +39,61 @@ public class Question3 : IAnswer
             }
         }
 
-        int runningTotal = 0;
-        foreach(var item in DuplicateItems)
+        return FindTotal(DuplicateItems);
+    }
+
+    public int ProcessAnswer2()
+    {
+        var lines = Util.LoadQuestionFile("question3.txt");
+        List<char> DuplicateItems = new();
+
+        for (int line_idx = 0; line_idx < lines.Count; line_idx += 3)
         {
-            if(char.IsUpper(item))
+            var backpackFirst = lines[line_idx];
+            var backpackSecond = lines[line_idx + 1];
+            var backpackThird = lines[line_idx + 2];
+            bool foundForGroup = false;
+
+            for (int i = 0; i < backpackFirst.Length; i++)
             {
-                runningTotal += Array.IndexOf(LowerCasePriorities, char.ToLower(item)) + 27;    
+                if (foundForGroup) break;
+                for (int j = 0; j < backpackSecond.Length; j++)
+                {
+                    if (foundForGroup) break;
+                    for (int k = 0; k < backpackThird.Length; k++)
+                    {
+                        if (
+                            backpackFirst[i] == backpackSecond[j] 
+                            && backpackFirst[i] == backpackThird[k] 
+                            && backpackSecond[j] == backpackThird[k]
+                        )
+                        {
+                            DuplicateItems.Add(backpackThird[k]);
+                            foundForGroup = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return FindTotal(DuplicateItems);
+    }
+
+    public int FindTotal(List<char> DuplicateItems)
+    {
+        int runningTotal = 0;
+        foreach (var item in DuplicateItems)
+        {
+            if (char.IsUpper(item))
+            {
+                runningTotal += Array.IndexOf(LowerCasePriorities, char.ToLower(item)) + 27;
                 continue;
             }
 
             int index = Array.IndexOf(LowerCasePriorities, item) + 1;
             runningTotal += index;
         }
-
         return runningTotal;
     }
 
@@ -59,5 +101,8 @@ public class Question3 : IAnswer
     {
         var answer = ProcessAnswer();
         Console.WriteLine($"Question 3: {answer} is the sum of the priorities of those item types");
+
+        var answer2 = ProcessAnswer2();
+        Console.WriteLine($"Question 3 part 2: {answer2} is the sum of the priorities of those item types");
     }
 }
